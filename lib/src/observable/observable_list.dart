e93b0de0f90d96b52b51763333febab71e1bae7c
Sergei Skotnikov <skotnikov@europe.com>
2021-12-12 17:38:23 +0300

    add_remove_last

--- lib/src/observable/observable_list.dart
+++ lib/src/observable/observable_list.dart
@@ -8,5 +8,6 @@ class ObservableList<T> extends Observable<List<T>> {
 
   void add(T value) => _val = _val..add(value);
   void remove(T value) => _val = _val..remove(value);
+  void removeLast() => _val = _val..removeLast();
   void addAll(Iterable<T> iterable) => _val = _val..addAll(iterable);
 }
