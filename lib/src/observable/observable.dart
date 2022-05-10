cb80ec2f8574673a4a94b61926120daac4067f0c
Sergei Skotnikov <skotnikov@europe.com>
2022-05-10 22:16:12 +0300

    ref

--- lib/src/observable/observable.dart
+++ lib/src/observable/observable.dart
@@ -1,26 +1,15 @@
 import 'package:flutter/foundation.dart';
 
-class Observable<T> implements ChangeNotifier {
+class Observable<T> {
   Observable(T value) : _value = ValueNotifier<T>(value);
 
-  late final ValueNotifier<T> _value;
+  final ValueNotifier<T> _value;
 
   T get value => _value.value;
 
   set value(T value) => _value.value = value;
 
-  @override
   void addListener(VoidCallback listener) => _value.addListener(listener);
 
-  @override
   void removeListener(VoidCallback listener) => _value.removeListener(listener);
-
-  @override
-  void dispose() => _value.dispose();
-
-  @override
-  bool get hasListeners => _value.hasListeners;
-
-  @override
-  void notifyListeners() => _value.notifyListeners();
 }
--- lib/src/observable/observable_list.dart
+++ lib/src/observable/observable_list.dart
@@ -1,149 +1,87 @@
+// ignore_for_file: invalid_use_of_visible_for_testing_member, invalid_use_of_protected_member
+
+import 'dart:collection';
 import 'dart:math';
 
 import 'package:easy_state/src/observable/observable.dart';
+import 'package:flutter/foundation.dart';
 
-class ObservableList<T> extends Observable<List<T>> implements List<T> {
-  ObservableList(List<T> value) : super(value);
+class ObservableList<T> with ListMixin<T> implements Observable<List<T>> {
+  ObservableList(List<T> value) : _value = ValueNotifier(value);
 
-  // ignore: invalid_use_of_protected_member, invalid_use_of_visible_for_testing_member
-  void _notify() => super.notifyListeners();
-
-  @override
-  T get first => super.value.first;
+  final ValueNotifier<List<T>> _value;
 
   @override
-  T get last => super.value.last;
+  int get length => _value.value.length;
 
   @override
-  int get length => super.value.length;
+  set length(int newLength) => _value.value.length = newLength;
 
   @override
-  List<T> operator +(List<T> other) {
-    super.value = super.value + other;
-    _notify();
-
-    return super.value;
-  }
-
-  @override
-  T operator [](int index) => super.value[index];
+  T operator [](int index) => _value.value[index];
 
   @override
   void operator []=(int index, T value) {
-    super.value[index] = value;
-    _notify();
+    _value.value[index] = value;
+    _value.notifyListeners();
   }
 
   @override
-  void add(T value) {
-    super.value.add(value);
-    _notify();
+  List<T> get value => this;
+
+  @override
+  set value(List<T> value) => _value.value = value;
+
+  @override
+  void addListener(VoidCallback listener) => _value.addListener(listener);
+
+  @override
+  void removeListener(VoidCallback listener) => _value.removeListener(listener);
+
+  @override
+  List<T> operator +(List<T> other) => _value.value = _value.value + other;
+
+  @override
+  void add(T element) {
+    _value.value.add(element);
+    _value.notifyListeners();
   }
 
   @override
   void addAll(Iterable<T> iterable) {
-    super.value.addAll(iterable);
-    _notify();
+    _value.value.addAll(iterable);
+    _value.notifyListeners();
   }
-
-  @override
-  bool any(bool Function(T element) test) => super.value.any(test);
-
-  @override
-  Map<int, T> asMap() => super.value.asMap();
-
-  @override
-  List<R> cast<R>() => super.value.cast<R>();
 
   @override
   void clear() {
-    super.value.clear();
-    _notify();
+    _value.value.clear();
+    _value.notifyListeners();
   }
 
   @override
-  bool contains(Object? element) => super.value.contains(element);
-
-  @override
-  T elementAt(int index) => super.value.elementAt(index);
-
-  @override
-  bool every(bool Function(T element) test) => super.value.every(test);
-
-  @override
-  Iterable<E> expand<E>(Iterable<E> Function(T element) toElements) => super.value.expand(toElements);
-
-  @override
-  void fillRange(int start, int end, [T? fillValue]) {
-    super.value.fillRange(start, end, fillValue);
-    _notify();
+  void fillRange(int start, int end, [T? fill]) {
+    _value.value.fillRange(start, end, fill);
+    _value.notifyListeners();
   }
-
-  @override
-  T firstWhere(bool Function(T element) test, {T Function()? orElse}) => super.value.firstWhere(test, orElse: orElse);
-
-  @override
-  E fold<E>(E initialValue, E Function(E previousValue, T element) combine) => super.value.fold(initialValue, combine);
-
-  @override
-  Iterable<T> followedBy(Iterable<T> other) => super.value.followedBy(other);
-
-  @override
-  void forEach(void Function(T element) action) => super.value.forEach(action);
-
-  @override
-  Iterable<T> getRange(int start, int end) => super.value.getRange(start, end);
-
-  @override
-  int indexOf(T element, [int start = 0]) => super.value.indexOf(element, start);
-
-  @override
-  int indexWhere(bool Function(T element) test, [int start = 0]) => super.value.indexWhere(test, start);
 
   @override
   void insert(int index, T element) {
-    super.value.insert(index, element);
-    _notify();
+    _value.value.insert(index, element);
+    _value.notifyListeners();
   }
 
   @override
   void insertAll(int index, Iterable<T> iterable) {
-    super.value.insertAll(index, iterable);
-    _notify();
+    _value.value.insertAll(index, iterable);
+    _value.notifyListeners();
   }
 
   @override
-  bool get isEmpty => super.value.isEmpty;
-
-  @override
-  bool get isNotEmpty => super.value.isNotEmpty;
-
-  @override
-  Iterator<T> get iterator => super.value.iterator;
-
-  @override
-  String join([String separator = ""]) => super.value.join(separator);
-
-  @override
-  int lastIndexOf(T element, [int? start]) => super.value.lastIndexOf(element, start);
-
-  @override
-  int lastIndexWhere(bool Function(T element) test, [int? start]) => super.value.lastIndexWhere(test, start);
-
-  @override
-  T lastWhere(bool Function(T element) test, {T Function()? orElse}) => super.value.lastWhere(test, orElse: orElse);
-
-  @override
-  Iterable<E> map<E>(E Function(T e) toElement) => super.value.map(toElement);
-
-  @override
-  T reduce(T Function(T value, T element) combine) => super.value.reduce(combine);
-
-  @override
-  bool remove(Object? value) {
-    final isRemoved = super.value.remove(value);
+  bool remove(Object? element) {
+    final isRemoved = _value.value.remove(value);
     if (isRemoved) {
-      _notify();
+      _value.notifyListeners();
     }
 
     return isRemoved;
@@ -151,12 +89,12 @@ class ObservableList<T> extends Observable<List<T>> implements List<T> {
 
   @override
   T removeAt(int index) {
-    final beforeLength = super.value.length;
-    final removeAt = super.value.removeAt(index);
-    final afterLength = super.value.length;
+    final beforeLength = _value.value.length;
+    final removeAt = _value.value.removeAt(index);
+    final afterLength = _value.value.length;
 
     if (beforeLength != afterLength) {
-      _notify();
+      _value.notifyListeners();
     }
 
     return removeAt;
@@ -164,12 +102,12 @@ class ObservableList<T> extends Observable<List<T>> implements List<T> {
 
   @override
   T removeLast() {
-    final beforeLength = super.value.length;
-    final removeLast = super.value.removeLast();
-    final afterLength = super.value.length;
+    final beforeLength = _value.value.length;
+    final removeLast = _value.value.removeLast();
+    final afterLength = _value.value.length;
 
     if (beforeLength != afterLength) {
-      _notify();
+      _value.notifyListeners();
     }
 
     return removeLast;
@@ -177,97 +115,49 @@ class ObservableList<T> extends Observable<List<T>> implements List<T> {
 
   @override
   void removeRange(int start, int end) {
-    super.value.removeRange(start, end);
-    _notify();
+    _value.value.removeRange(start, end);
+    _value.notifyListeners();
   }
 
   @override
   void removeWhere(bool Function(T element) test) {
-    super.value.removeWhere(test);
-    _notify();
+    _value.value.removeWhere(test);
+    _value.notifyListeners();
   }
 
   @override
-  void replaceRange(int start, int end, Iterable<T> replacements) {
-    super.value.replaceRange(start, end, replacements);
-    _notify();
+  void replaceRange(int start, int end, Iterable<T> newContents) {
+    _value.value.replaceRange(start, end, newContents);
+    _value.notifyListeners();
   }
 
   @override
   void retainWhere(bool Function(T element) test) {
-    super.value.retainWhere(test);
-    _notify();
+    _value.value.retainWhere(test);
+    _value.notifyListeners();
   }
 
   @override
-  Iterable<T> get reversed => super.value.reversed;
-
-  @override
   void setAll(int index, Iterable<T> iterable) {
-    super.value.setAll(index, iterable);
-    _notify();
+    _value.value.setAll(index, iterable);
+    _value.notifyListeners();
   }
 
   @override
   void setRange(int start, int end, Iterable<T> iterable, [int skipCount = 0]) {
-    super.value.setRange(start, end, iterable, skipCount);
-    _notify();
+    _value.value.setRange(start, end, iterable, skipCount);
+    _value.notifyListeners();
   }
 
   @override
   void shuffle([Random? random]) {
-    super.value.shuffle(random);
-    _notify();
+    _value.value.shuffle(random);
+    _value.notifyListeners();
   }
-
-  @override
-  T get single => super.value.single;
-
-  @override
-  T singleWhere(bool Function(T element) test, {T Function()? orElse}) => super.value.singleWhere(test, orElse: orElse);
-
-  @override
-  Iterable<T> skip(int count) => super.value.skip(count);
-
-  @override
-  Iterable<T> skipWhile(bool Function(T value) test) => super.value.skipWhile(test);
 
   @override
   void sort([int Function(T a, T b)? compare]) {
-    super.value.sort();
-    _notify();
+    _value.value.sort();
+    _value.notifyListeners();
   }
-
-  @override
-  List<T> sublist(int start, [int? end]) => super.value.sublist(start, end);
-
-  @override
-  Iterable<T> take(int count) => super.value.take(count);
-
-  @override
-  Iterable<T> takeWhile(bool Function(T value) test) => super.value.takeWhile(test);
-
-  @override
-  List<T> toList({bool growable = true}) => super.value.toList(growable: growable);
-
-  @override
-  Set<T> toSet() => super.value.toSet();
-
-  @override
-  Iterable<T> where(bool Function(T element) test) => super.value.where(test);
-
-  @override
-  Iterable<E> whereType<E>() => super.value.whereType<E>();
-
-  @override
-  set first(T value) => super.value.first;
-
-  @override
-  set last(T value) => super.value.last;
-
-  @override
-  set length(int newLength) => value.length;
-
-  @override
-  List<T> get value => this;
 }
